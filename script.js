c2c3a25e00538a21c3309d805cae5b56f1431273
Andrei <catrina.andrei93@yahoo.com>
2021-12-07 20:36:04 +0200

    -sign-up-form-switch

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let myFunction = function () {
+let switchForms = function () {
   // console.log("Click");
-  let element = document.getElementById("sign-up-form");
+  let element = document.getElementById("sign-in-form");
   console.log(element);
   element.classList.add("hidden");
+
+  let element2 = document.getElementById("sign-up-form");
+  console.log(element2);
+  element2.classList.remove("hidden");
 };
